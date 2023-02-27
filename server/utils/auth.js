c407e1fd1ef0ea0f565f5a9f036f0af160ba5dec
Christian Areas <me@areas.me>
2023-02-27 00:30:05 -0800

    Refactored auth util for better readability.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,48 @@
-const jwt = require('jsonwebtoken');
+// Dependencies.
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+// JWT token secret and expiration.
+const secret = process.env.JWT_SECRET
+const expiration = process.env.JWT_EXPIRATION
+
+async function authMiddleware(req, res, next) {
+  try {
+    // Get the token from the query param or header.
+    let token = req.query.token || req.headers.authorization
+    // If the token’s from the header, split and pop it (remove “Bearer”), and trim any whitespace.
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim()
+    }
+    // If there’s no token, return an error.
+    if (!token) {
+      return res.status(401).json("You must submit a token.")
+    }
+    // Verify the token.
+    const { data } = jwt.verify(
+      token,
+      secret,
+      { maxAge: expiration }
+    )
+    // Pass the user data to the next step.
+    req.user = data
+    next()
+  } catch (err) {
+    console.log(err)
+    res.status(500).json("Sorry, something went wrong.")
+  }
+}
+
+function signToken({ _id, username, email }) {
+  // Sign the token.
+  return jwt.sign(
+    { data: { _id, username, email } },
+    secret,
+    { expiresIn: expiration }
+  )
+}
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+  authMiddleware,
+  signToken,
+}
